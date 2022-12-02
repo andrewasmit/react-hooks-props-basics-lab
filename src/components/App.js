b0992ef61e618677032fb1740925026dd7b58196
Andrew <andrewasmit@hotmail.com>
2022-12-02 14:03:16 -0600

    commit tree b693299b12ac8bd81e748ef6d554cacf29ed7512

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,15 @@ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+// import user from "../data/user";
+// console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name="Liza" color ="firebrick" city = "New York" />
+      <About bio="I made this!" github="https://github.com/liza" linkedin="https://www.linkedin.com/in/liza/"/>
     </div>
   );
 }
